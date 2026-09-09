588f7ac9142efcd33775dbddc6e2ecbd9a4d7dcb
Darani Wickramasinghe <daraniwickramasinghe@gmail.com>
2026-09-09 20:59:59 +0530

    fix: restore passing test

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,7 +7,7 @@ client = TestClient(app)
 def test_root():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello CI Pipeline BROKEN"}
+    assert response.json() == {"message": "Hello CI Pipeline"}
 
 
 def test_health():
